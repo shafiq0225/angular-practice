712d4d10a2ed6ce0d90a45c881f45a35f47264ce
shafiq0225 <shafiqahamed.be@gmail.com>
2024-10-23 20:27:17 +0530

    Building component and communicating

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./card/card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { title: 'Neat Tree', imageUrl: 'assets/tree.jpeg', username: 'nature', content: 'I saw this neat tree today' },
+    { title: 'Snowy Mountain', imageUrl: 'assets/mountain.jpeg', username: 'mountainlover', content: 'Here is a picture of a snowy mountain' },
+    { title: 'Mountain Biking', imageUrl: 'assets/biking.jpeg', username: 'biking12222', content: 'I did some bikng today' }
+  ]
 }
